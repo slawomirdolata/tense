e45b2ee3d42909ab995ca722aa14b019db0b7455
Slawek <slawek@boo.pl>
2021-07-28 16:54:51 +0200

    Instalacja SLIM. Routing cz. I

---
+++ app/dependencies.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+use App\Application\Settings\SettingsInterface;
+use DI\ContainerBuilder;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Monolog\Processor\UidProcessor;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+
+return function (ContainerBuilder $containerBuilder) {
+    $containerBuilder->addDefinitions([
+        LoggerInterface::class => function (ContainerInterface $c) {
+            $settings = $c->get(SettingsInterface::class);
+
+            $loggerSettings = $settings->get('logger');
+            $logger = new Logger($loggerSettings['name']);
+
+            $processor = new UidProcessor();
+            $logger->pushProcessor($processor);
+
+            $handler = new StreamHandler($loggerSettings['path'], $loggerSettings['level']);
+            $logger->pushHandler($handler);
+
+            return $logger;
+        },
+
+        PDO::class => function (ContainerInterface $c) {
+            $settings = $c->get(SettingsInterface::class);
+            $dbSettings = $settings->get('db');
+ 
+//            $host = $dbSettings['host'];
+//            $dbname = $dbSettings['database'];
+            $username = $dbSettings['username'] ?? null;
+            $password = $dbSettings['password'] ?? null;
+//            $charset = $dbSettings['charset'];
+//            $flags = $dbSettings['flags'];
+//            $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+            $dsn = $dbSettings['driver'] . ':' . $dbSettings['database'];
+            return new PDO($dsn, $username, $password);
+        },
+    ]);
+};
